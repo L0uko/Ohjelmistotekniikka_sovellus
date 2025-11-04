1aefa2eb245cc6b7a12aa69c399fe2945eab963c
Touko Toivonen <touko.t.toivonen@gmail.com>
2025-11-04 19:42:19 +0200

    change .gitignore and make unittests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,21 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(self.maksukortti.saldo,1000)
+
+    def test_rahan_lisays(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(self.maksukortti.saldo,1100)
+
+    def test_rahan_ottaminen(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo,500)
+    
+    def test_rahan_ottaminen_liian_vahan(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(self.maksukortti.saldo,1000)
