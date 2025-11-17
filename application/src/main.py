9f79deb27e116af4cc22821aff27233aaba52a0c
Touko Toivonen <touko.t.toivonen@gmail.com>
2025-11-17 16:35:05 +0200

    invoke commands work

--- application/src/main.py
+++ application/src/main.py
@@ -2,5 +2,6 @@ import tk as tk
 import cowsay
 import gamelogic as gamelogic
 
+field = gamelogic.Map(10,20)
+print(field.return_map())
 cowsay.tux("TkT Inter")
-
--- application/src/tests/gamelogic_test.py
+++ application/src/tests/gamelogic_test.py
@@ -6,7 +6,6 @@ class TestMap(unittest.TestCase):
     def setUp(self):
         self.field = Map(10,20)
     def test_map_right_size(self):
-        #self.field.return_map()
         self.assertEqual(len(self.field.return_map()),20)
         for row in self.field.return_map():
             self.assertEqual(len(row),10)
