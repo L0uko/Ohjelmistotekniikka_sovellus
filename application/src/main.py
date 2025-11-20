251281695f170dde7fbd5436e91ddfc7a4ef6b1f
Louko <touko.t.toivonen@gmail.com>
2025-11-20 13:43:27 +0200

    update tkinter problem

--- application/src/main.py
+++ application/src/main.py
@@ -1,12 +1,8 @@
-import tk as tk # pyright: ignore[reportMissingImports]
+import tkinter  # pyright: ignore[reportMissingImports]
 import cowsay # pyright: ignore[reportMissingImports]
 import gamelogic as gamelogic
 
 field = gamelogic.Map(10,20)
 cowsay.tux("TkT Inter")
-root = tk()
-frm = tk.ttk.Frame(root, padding=10)
-frm.grid()
-tk.ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-tk.ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+root = tkinter.Tk()
+root.geometry("500x500")
