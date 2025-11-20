c2c9189983b1c2dafed67fe801361604dc5435ba
Touko Toivonen <touko.t.toivonen@gmail.com>
2025-11-20 15:29:40 +0200

    fix tkinter and add ui.py

--- application/src/main.py
+++ application/src/main.py
@@ -1,8 +1,10 @@
-import tkinter  # pyright: ignore[reportMissingImports]
+import tkinter # pyright: ignore[reportMissingImports]
 import cowsay # pyright: ignore[reportMissingImports]
-import gamelogic as gamelogic
+import gamelogic # pyright: ignore[reportMissingImports]
 
 field = gamelogic.Map(10,20)
 cowsay.tux("TkT Inter")
 root = tkinter.Tk()
+root.title("Tetris")
 root.geometry("500x500")
+root.mainloop()
---
+++ application/src/ui.py
@@ -0,0 +1,3 @@
+import gamelogic 
+import main 
+
